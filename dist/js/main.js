ecd76480147e4069caae464e264e51980eac20c3
ZhouTian0716 <egglord0716@gmail.com>
2022-12-10 16:18:58 +1100

    navigation done

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,15 @@
-console.log('hi')
+const primaryNav = document.querySelector("#primary-navigation");
+const navToggleBtn = document.querySelector("#btn__nav__toggle");
+
+navToggleBtn.addEventListener("click", () => {
+  const visible = primaryNav.getAttribute("data-visible");
+  if (visible === "false") {
+    primaryNav.setAttribute("data-visible", "true");
+    navToggleBtn.setAttribute("aria-expanded", "true");
+    navToggleBtn.classList.replace("fa-bars", "fa-close");
+  } else {
+    primaryNav.setAttribute("data-visible", "false");
+    navToggleBtn.setAttribute("aria-expanded", "false");
+    navToggleBtn.classList.replace("fa-close", "fa-bars");
+  }
+});
